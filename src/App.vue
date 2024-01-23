f87b79d3e06f78e131ecdcc6dcf2ae4c0a696b6d
langochai <langochai11121998@gmail.com>
2024-01-23 14:10:53 +0700

    learned vue routing

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,29 @@
 </script>
 
 <template>
-    <example></example>
+    <router-link to="/food">Food</router-link>
+    <br>
+    <router-link to="/animal">Animal</router-link>
+    <router-view></router-view>
 </template>
 
 <style scoped>
-
+  a {
+    display: inline-block;
+    background-color: black;
+    border: solid 1px black;
+    color: white;
+    padding: 5px;
+    margin: 10px;
+  }
+  a:hover,
+  a.router-link-active {
+    background-color: rgb(110, 79, 13);
+  }
+  div {
+    border: dashed black 1px;
+    padding: 20px;
+    margin: 10px;
+    display: inline-block;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import DashBoard from './components/DashBoard.vue'
-const app = createApp(App)
+import { createApp } from "vue";
+import App from "./App.vue";
+import DashBoard from "./components/DashBoard.vue";
+import Food from "./components/Food.vue";
+import Animal from "./components/Animal.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-
-app.component('example',DashBoard)
-app.mount('#app')
+const app = createApp(App);
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/food", component: Food },
+        { path: "/animal", component: Animal },
+    ],
+});
+app.use(router)
+// app.component("example", DashBoard);
+app.mount("#app");
